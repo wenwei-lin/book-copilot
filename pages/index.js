cb4b8f6f306223f6b821799d00046c7481ab4692
Wenwei Lin <wenwei66.lin@gmail.com>
2023-08-20 03:13:50 +0800

    add related documents

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,10 @@ import styles from '../styles/Home.module.css'
 import Image from 'next/image'
 import ReactMarkdown from 'react-markdown'
 import CircularProgress from '@mui/material/CircularProgress';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import { Button } from '@mui/material'
 
 export default function Home() {
 
@@ -16,6 +20,10 @@ export default function Home() {
       "role": "assistant"
     }
   ]);
+
+  // Use for upload file
+  const [uploading, setUploading] = useState(false);
+  const [selectedFile, setSelectedFile] = useState(null);
 
   const messageListRef = useRef(null);
   const textAreaRef = useRef(null);
@@ -31,6 +39,36 @@ export default function Home() {
     textAreaRef.current.focus();
   }, []);
 
+  // Handle file
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(file);
+  };
+
+  const handleUpload = async () => {
+    if (selectedFile) {
+      const formData = new FormData();
+      formData.append('file', selectedFile);
+      console.log(formData)
+
+      console.log("Upload file")
+      // You can send formData to your server using fetch or a library like axios.
+      // Example: 
+      // await fetch('/api/upload', {
+      //   method: 'POST',
+      //   body: formData,
+      // });
+      await fetch('/api/upload', {
+        method: 'POST',
+        body: formData,
+      })
+      .then(() => console.log("File uploaded."))
+
+      // Reset selectedFile state after successful upload
+      setSelectedFile(null);
+    }
+  };
+
   // Handle errors
   const handleError = () => {
     setMessages((prevMessages) => [...prevMessages, { "content": "Oops! There seems to be an error. Please try again.", "role": "assistant" }]);
@@ -39,7 +77,7 @@ export default function Home() {
   }
 
   // Handle form submission
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (userInput.trim() === "") {
@@ -53,7 +91,7 @@ export default function Home() {
     const response = await fetch("/api/chat", {
       method: "POST",
       headers: {
-          "Content-Type": "application/json",
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ query: userInput, history: history }),
     });
@@ -61,7 +99,7 @@ export default function Home() {
     if (!response.ok) {
       handleError();
       return;
-  }
+    }
 
     // Reset user input
     setUserInput("");
@@ -74,13 +112,13 @@ export default function Home() {
 
     setMessages((prevMessages) => [...prevMessages, { "content": data.result.answer, "role": "assistant", "docs": data.result.docs }]);
     setLoading(false);
-    
+
   };
 
   // Prevent blank submissions and allow for multiline input
   const handleEnter = (e) => {
     if (e.key === "Enter" && userInput) {
-      if(!e.shiftKey && userInput) {
+      if (!e.shiftKey && userInput) {
         handleSubmit(e);
       }
     } else if (e.key === "Enter") {
@@ -103,7 +141,7 @@ export default function Home() {
       ]);
 
     }
-    }, [messages])
+  }, [messages])
 
   return (
     <>
@@ -114,67 +152,93 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={styles.topnav}>
-      <div className = {styles.navlogo}>
-    <a href="/">读书助理</a>
-    </div>
-    <div className = {styles.navlinks}>
-    <a href="https://langchain.readthedocs.io/en/latest/" target="_blank">Docs</a>
-    <a href="https://github.com/zahidkhawaja/langchain-chat-nextjs" target="_blank">GitHub</a>
-    </div>
-</div>
-      <main className={styles.main}>
-      <div className = {styles.cloud}>
-        <div ref={messageListRef} className = {styles.messagelist}>
-        {messages.map((message, index) => {
-          return (
-            // The latest message sent by the user will be animated while waiting for a response
-              <div key = {index} className = {message.role === "user" && loading && index === messages.length - 1  ? styles.usermessagewaiting : message.role === "assistant" ? styles.apimessage : styles.usermessage}>
-                {/* Display the correct icon depending on the message type */}
-                {message.role === "assistant" ? <Image src = "/parroticon.png" alt = "AI" width = "30" height = "30" className = {styles.boticon} priority = {true} /> : <Image src = "/usericon.png" alt = "Me" width = "30" height = "30" className = {styles.usericon} priority = {true} />}
-              <div className = {styles.markdownanswer}>
-                {/* Messages are being rendered in Markdown format */}
-                <ReactMarkdown linkTarget = {"_blank"}>{message.content}</ReactMarkdown>
-                </div>
-              </div>
-          )
-        })}
+        <div className={styles.navlogo}>
+          <a href="/">读书助理</a>
         </div>
-            </div>
-           <div className={styles.center}>
-            
-            <div className = {styles.cloudform}>
-           <form onSubmit = {handleSubmit}>
-          <textarea 
-          disabled = {loading}
-          onKeyDown={handleEnter}
-          ref = {textAreaRef}
-          autoFocus = {false}
-          rows = {1}
-          maxLength = {512}
-          type="text" 
-          id="userInput" 
-          name="userInput" 
-          placeholder = {loading? "Waiting for response..." : "Type your question..."}  
-          value = {userInput} 
-          onChange = {e => setUserInput(e.target.value)} 
-          className = {styles.textarea}
-          />
-            <button 
-            type = "submit" 
-            disabled = {loading}
-            className = {styles.generatebutton}
-            >
-            {loading ? <div className = {styles.loadingwheel}><CircularProgress color="inherit" size = {20}/> </div> : 
-            // Send icon SVG in input field
-            <svg viewBox='0 0 20 20' className={styles.svgicon} xmlns='http://www.w3.org/2000/svg'>
-            <path d='M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z'></path>
-          </svg>}
-            </button>
+        <div className={styles.navlinks}>
+          {/** TODO: upload file */}
+        </div>
+      </div>
+      <main className={styles.main}>
+        <div className={styles.cloud}>
+          <div ref={messageListRef} className={styles.messagelist}>
+            {messages.map((message, index) => {
+              return (
+                // The latest message sent by the user will be animated while waiting for a response
+                <div key={index} className={message.role === "user" && loading && index === messages.length - 1 ? styles.usermessagewaiting : message.role === "assistant" ? styles.apimessage : styles.usermessage}>
+                  {/* Display the correct icon depending on the message type */}
+                  {message.role === "assistant" ? <Image src="/parroticon.png" alt="AI" width="30" height="30" className={styles.boticon} priority={true} /> : <Image src="/usericon.png" alt="Me" width="30" height="30" className={styles.usericon} priority={true} />}
+                  <div className={styles.markdownanswer}>
+                    {/* Messages are being rendered in Markdown format */}
+                    <ReactMarkdown linkTarget={"_blank"}>{message.content}</ReactMarkdown>
+                    {
+                      message.docs && message.docs.length > 0 &&
+                      <Accordion className={styles.docs}>
+                        <AccordionSummary
+                          expandIcon={<svg viewBox='0 0 20 20' className={styles.svgicon} xmlns='http://www.w3.org/2000/svg'>
+                            <path d='M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z'></path>
+                          </svg>}
+                          aria-controls="panel1a-content"
+                          id="panel1a-header"
+                        >
+                          <h3>相关文档</h3>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                          <div className={styles.docslist}>
+                            {message.docs.map((doc, index) => {
+                              return (
+                                <div key={index} className={styles.doc}>
+                                  <div className={styles.doccontent}>
+                                    <ReactMarkdown linkTarget={"_blank"}>{doc}</ReactMarkdown>
+                                  </div>
+                                </div>
+                              )
+                            })}
+                          </div>
+                        </AccordionDetails>
+                      </Accordion>
+                    }
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+        <div className={styles.center}>
+
+          <div className={styles.cloudform}>
+            <form onSubmit={handleSubmit}>
+              <textarea
+                disabled={loading}
+                onKeyDown={handleEnter}
+                ref={textAreaRef}
+                autoFocus={false}
+                rows={1}
+                maxLength={512}
+                type="text"
+                id="userInput"
+                name="userInput"
+                placeholder={loading ? "Waiting for response..." : "Type your question..."}
+                value={userInput}
+                onChange={e => setUserInput(e.target.value)}
+                className={styles.textarea}
+              />
+              <button
+                type="submit"
+                disabled={loading}
+                className={styles.generatebutton}
+              >
+                {loading ? <div className={styles.loadingwheel}><CircularProgress color="inherit" size={20} /> </div> :
+                  // Send icon SVG in input field
+                  <svg viewBox='0 0 20 20' className={styles.svgicon} xmlns='http://www.w3.org/2000/svg'>
+                    <path d='M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z'></path>
+                  </svg>}
+              </button>
             </form>
-            </div>
-            <div className = {styles.footer}>
-            <p>Powered by <a href = "https://github.com/hwchase17/langchain" target="_blank">LangChain</a>. Built by <a href="https://twitter.com/chillzaza_" target="_blank">Zahid</a>.</p>
-            </div>
+          </div>
+          <div className={styles.footer}>
+            <p>Powered by <a href="https://github.com/hwchase17/langchain" target="_blank">LangChain</a>. Built by <a href="https://twitter.com/chillzaza_" target="_blank">Zahid</a>.</p>
+          </div>
         </div>
       </main>
     </>
